4470378a5b917c43c98d2edaf3d2b1bcbed35c1a
Alessandro <alecaragiulo@gmail.com>
2022-05-09 23:43:27 -0300

    TP1 vfinal

---
+++ tpsysl1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main () {
+
+    char caracter;
+
+    printf("ingrese texto");
+    caracter = getchar ();
+
+    while (caracter !='\n')
+    {
+        if (isalpha(caracter)){
+
+            if (islower(caracter))
+                printf("%c",toupper(caracter));
+
+
+            else
+                printf("%c",tolower(caracter));      
+        }
+
+        if (!isalnum(caracter))
+            printf("%c",caracter);
+        
+        caracter = getchar ();
+    }
+
+return 0;
+}
